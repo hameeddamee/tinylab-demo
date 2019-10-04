f9962960f9c5016517a62bd16270b63250f798bb
Hameed Damee <dameeimpact@gmail.com>
2019-10-04 14:58:37 +0100

    image import error removed

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,19 +1,8 @@
 import React from "react";
 import Login from "../../components/Login/Login";
-import LoginImage from "../../images/bg-01.jpg";
 
 const LoginPage = () => {
-  return (
-    // <div className="limiter">
-    <div className="container-login100">
-      <div
-        className="login100-more"
-        style={{ backgroundImage: `url(${LoginImage})` }}
-      ></div>
-      <Login></Login>
-    </div>
-    // </div>
-  );
+  return <Login />;
 };
 
 export default LoginPage;
